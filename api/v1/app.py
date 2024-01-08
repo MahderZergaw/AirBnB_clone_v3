998f06649ccfe01270daa3ca23e68b67b40aa10c
MahderZergaw <mahderzergaw640@gmail.com>
2024-01-08 12:19:46 -0800

    gri

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+Create Flask app; and register the blueprint app_views to Flask instance app.
+'''
+from flask import Flask
+from os import getenv
+from models import storage
+from api.v1.views import app_views
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    '''
+    function to tear down app context so that the storage is closed
+    '''
+    storage.close()
+if __name__ == '__main__':
+    host = getenv(HBNB_API_HOST, 0.0.0.0)
+    port = int(getenv(HBNB_API_PORT, 5000))
+    app.run(host=host, port=port, threaded=True)
