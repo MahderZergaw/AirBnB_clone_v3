a8906f7d244c84c3e6efc5464f944f8da7069821
MahderZergaw <mahderzergaw640@gmail.com>
2024-01-09 13:41:36 -0800

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-'''
-Create Flask app; and register the blueprint app_views to Flask instance app.
-'''
-from flask import Flask, jsonify
-from os import getenv
+"""return the status of your API
+"""
+
+from flask import Flask
 from models import storage
+from os import getenv
 from api.v1.views import app_views
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
-    '''
-    function to tear down app context so that the storage is closed
-    '''
+    """close storage
+    """
     storage.close()
 
 
-@app.errorhandler(404)
-def not_found():
-    '''
-    error response for the not found or error code 404
-    '''
-    return jsonify({"error": "Not found"}), 404
-if __name__ == '__main__':
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+if __name__ == "__main__":
+    HBNB_API_HOST = getenv('HBNB_API_HOST')
+    HBNB_API_PORT = getenv('HBNB_API_PORT')
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT,
+            threaded=True, debug=True)
