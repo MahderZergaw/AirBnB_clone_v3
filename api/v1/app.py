528a8636ac897ed24b5f88b42353f2b26501d695
MahderZergaw <mahderzergaw640@gmail.com>
2024-01-09 05:43:37 -0800

    error404

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 '''
 Create Flask app; and register the blueprint app_views to Flask instance app.
 '''
-from flask import Flask
+from flask import Flask, jsonify
 from os import getenv
 from models import storage
 from api.v1.views import app_views
@@ -16,6 +16,14 @@ def teardown_appcontext(exception):
     function to tear down app context so that the storage is closed
     '''
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found():
+    '''
+    error response for the not found or error code 404
+    '''
+    return jsonify({"error": "Not found"}), 404
 if __name__ == '__main__':
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(getenv('HBNB_API_PORT', 5000))
