3777d79624f5b0880e9bf19355e6e9b52bebadc0
MahderZergaw <mahderzergaw640@gmail.com>
2024-01-09 08:34:17 -0800

    i

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,32 @@
-from flask import Blueprint, jsonify
+#!/usr/bin/python3
+"""
+Blueprint for api status
+"""
 
-# Create a Blueprint named app_views
-app_views = Blueprint('app_views', __name__)
+from api.v1.views import app_views
+from flask import jsonify
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
-# Define a route /status on the app_views Blueprint
 
-
-@app_views.route('/status')
+@app_views.route("/status", methods=["GET"])
 def status():
+    """Returns the api status in JSON format"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=["GET"])
+def stats():
+    """Returns the number of items in storage in JSON format"""
+    from models import storage
+    classes = {"amenities": Amenity, "cities": City, "places": Place,
+               "reviews": Review, "states": State, "users": User
+               }
+
+    all_cls_stats = {key: storage.count(val) for key, val in classes.items()}
+    return jsonify(all_cls_stats)
