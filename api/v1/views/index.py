7f934a27d4d8721a2e10ed2e3a6b0a8a29f916b3
MahderZergaw <mahderzergaw640@gmail.com>
2024-01-09 01:34:22 -0800

    got

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-'''
+"""
 Create a route `/status` on the object app_views.
-'''
+"""
 from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
-def api_status():
-    '''
+@app_views.get('/status')
+def status():
+    """
     Returns a JSON response for RESTful API health.
-    '''
+    """
     response = {'status': 'OK'}
 
     return jsonify(response)
