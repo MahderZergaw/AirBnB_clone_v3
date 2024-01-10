c4926281f35b14dfdf14dc0b87da78ca6c2f03a8
root <tosinezekiel03@gmail.com>
2024-01-10 10:46:10 +0000

    Update api

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,8 +6,8 @@ from flask import Flask
 from os import getenv
 from models import storage
 from api.v1.views import app_views
+
 app = Flask(__name__)
-app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
@@ -16,7 +16,9 @@ def teardown_appcontext(exception):
     function to tear down app context so that the storage is closed
     '''
     storage.close()
+
 if __name__ == '__main__':
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(getenv('HBNB_API_PORT', 5000))
+    app.register_blueprint(app_views, url_prefix="/api/v1")
     app.run(host=host, port=port, threaded=True)
